cc3e4cdc66f9569eb51563e2b2efd1d86a206e9c
gnishida <gnishida@purdue.edu>
2020-03-08 11:27:30 -0700

    Initial commit

---
+++ QtTest/MainWindow.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <QtWidgets/QMainWindow>
+#include <QGuiApplication>
+
+#include <Qt3DCore/QEntity>
+#include <Qt3DRender/QCamera>
+#include <Qt3DRender/QCameraLens>
+#include <Qt3DCore/QTransform>
+#include <Qt3DCore/QAspectEngine>
+
+#include <Qt3DInput/QInputAspect>
+
+#include <Qt3DRender/QRenderAspect>
+#include <Qt3DExtras/QForwardRenderer>
+#include <Qt3DExtras/QPhongMaterial>
+#include <Qt3DExtras/QCylinderMesh>
+#include <Qt3DExtras/QSphereMesh>
+#include <Qt3DExtras/QTorusMesh>
+
+#include <QPropertyAnimation>
+
+#include "qt3dwindow.h"
+#include "orbittransformcontroller.h"
+#include "qorbitcameracontroller.h"
+
+#include "ui_MainWindow.h"
+
+class MainWindow : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	MainWindow(QWidget *parent = Q_NULLPTR);
+
+private:
+	Ui::MainWindowClass ui;
+
+	Qt3DExtras::Qt3DWindow view;
+};
